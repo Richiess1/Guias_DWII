e13f84f4c0f3e72431529b2b104820e1f8fc7539
Richiess1 <20235894@esen.edu.sv>
2025-02-03 21:01:48 -0600

    fin

--- Guia_2_3/src/App.jsx
+++ Guia_2_3/src/App.jsx
@@ -1,46 +1,71 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
-import {db} from './data/db'
+import { db } from './data/db'
 import { Guitar } from './components/Guitar'
+
 
 
 function App() {
 
-  const [data, setData]= useState(db);
+  function initialCart() {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
 
-  const [cart, setCart] = useState([]);
+  const [data, setData] = useState(db);
 
-  function addToCart (guitar){
-    const itemIndex = cart.findIndex((item)=>guitar.id===item.id)
-    if(itemIndex===-1){
+  const [cart, setCart] = useState(initialCart);
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
+
+  function addToCart(guitar) {
+    const itemIndex = cart.findIndex((item) => guitar.id === item.id)
+    if (itemIndex === -1) {
       guitar.quantity = 1;
       setCart([...cart, guitar])
-    } else{
+    } else {
       const updatedCart = [...cart]
       updatedCart[itemIndex].quantity++;
       setCart(updatedCart);
     }
+    saveCartToLocalStorage()
   }
-  
+
+  function calculateTotal() {
+    /*let total= 0;
+    for (const guitar of cart){
+      total+=guitar.price*guitar.quantity;
+    }*/
+    let total = cart.reduce((total, item) => total + item.price * item.quantity, 0)
+    return total;
+  }
+
+
+
+
+
+
 
   return (
     <>
-      <Header cart={cart}/>
+      <Header cart={cart} total={calculateTotal()} />
 
       <main className="container-xl mt-5">
-          <h2 className="text-center">Nuestra Colección</h2>
+        <h2 className="text-center">Nuestra Colección</h2>
 
-          <div className="row mt-5">
-            {data.map((guitar)=>(
-              <Guitar guitar={guitar} key={guitar.id} addToCart={addToCart} />
-            ))}
-          
-          </div>
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar guitar={guitar} key={guitar.id} addToCart={addToCart} />
+          ))}
+
+        </div>
       </main>
-      <Footer/>
+      <Footer />
 
-    
+
     </>
   )
 }
