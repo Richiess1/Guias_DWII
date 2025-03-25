1316619d1d4d9297d6f27cdb75da3a95c7e23458
Richiess1 <20235894@esen.edu.sv>
2025-03-25 10:17:38 -0600

    Update auth.ts

--- practica6_20235894/src/utils/auth.ts
+++ practica6_20235894/src/utils/auth.ts
@@ -4,6 +4,6 @@ export async function hashPassword(plainPassword: string): Promise<string>{
     return await argon2.hash(plainPassword);
 }
 
-export async function validatePassword(plainPassword: string, password: string): Promise<string>{
-    return await argon2.hash(plainPassword);
+export async function validatePassword(plainPassword: string, hashPassword: string): Promise<boolean>{
+    return await argon2.verify( hashPassword, plainPassword);
 };
